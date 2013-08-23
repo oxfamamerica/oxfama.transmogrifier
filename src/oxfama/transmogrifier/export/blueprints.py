a17875365cb9ac98d323566241d00458bf1e4d7d
cewing <cris@crisewing.com>
2013-08-23 15:16:38 -0700

    create a file writer section that exports all binary file field data to the filesystem in a hierarchical structure mirroring the structure of the Plone site.

--- src/oxfama/transmogrifier/export/blueprints.py
+++ src/oxfama/transmogrifier/export/blueprints.py
@@ -11,9 +11,16 @@ from Products.Archetypes.interfaces import IBaseObject
 from collective.transmogrifier.interfaces import ISection, ISectionBlueprint
 from collective.transmogrifier.utils import defaultMatcher
 from quintagroup.transmogrifier.sitewalker import SiteWalkerSection
+from quintagroup.transmogrifier.writer import WriterSection
 
 from oxfama.transmogrifier.export.unicode_csv import UnicodeDictWriter
 from oxfama.transmogrifier.export.unicode_csv import force_unicode
+
+try:
+    from plone.app.blob.interfaces import IBlobField
+    HAS_BLOB = True
+except ImportError:
+    HAS_BLOB = False
 
 
 class CountLimitedSitewalkerSection(SiteWalkerSection):
@@ -173,6 +180,98 @@ class SchemaDictionarySection(object):
                 ptype = 'unknown type'
             return_values.append(':'.join((ptype, identifier)))
         return return_values
+
+
+class FileWriterSection(WriterSection):
+    """writes files stored in file fields out to disk
+
+    Based on the quintagroup.transmogrifier WriterSection
+    """
+    classProvides(ISectionBlueprint)
+    implements(ISection)
+
+    def __init__(self, transmogrifier, name, options, previous):
+        super(FileWriterSection, self).__init__(
+            transmogrifier, name, options, previous)
+        self.pathkey = defaultMatcher(options, 'path-key', name, 'path')
+
+    def __iter__(self):
+        for item in self.previous:
+            pathkey = self.pathkey(*item.keys())[0]
+
+            if not pathkey:
+                yield item; continue
+
+            path = item[pathkey]
+            obj = self.context.unrestrictedTraverse(str(path), None)
+            if obj is None:         # path doesn't exist
+                yield item; continue
+
+            files = []
+            if IBaseObject.providedBy(obj):
+                # grab schema of item and convert it to a dict
+                obj_fields = [f.__name__ for f in obj.Schema().fields()]
+                for field in obj_fields:
+                    if obj.isBinary(field):
+                        fname, ct, data = self.extractFile(obj, field)
+                        if fname == '' or data == '':
+                            # empty file fields have empty filename and empty data
+                            # skip them
+                            continue
+                        files.append(dict(filename=fname, mimetype=ct, data=data))
+                    else:
+                        # this is not a file field, so skip it
+                        continue
+            
+            if files:
+                # write out files at this point
+                if len(files) > 1:
+                    # there is more than one file, we will create a folder at
+                    # the path of the current item and export all files there.
+                    item_path = path
+                else:
+                    # there is only one file, we can write it into the 
+                    # container for the current item (item['_path'][:-1])
+                    item_path, dummy = os.path.split(path)
+                for file_info in files:
+                    try:
+                        self.export_context.writeDataFile(file_info['filename'],
+                                                          file_info['data'],
+                                                          file_info['mimetype'],
+                                                          subdir=item_path)
+                    except TypeError, e:
+                        import pdb; pdb.set_trace( )
+                        raise
+
+            yield item
+
+    # stolen from q.t.binary
+    def extractFile(self, obj, field):
+        """ Return tuple of (filename, content_type, data)
+        """
+        field = obj.getField(field)
+        if HAS_BLOB and IBlobField.providedBy(field):
+            wrapper = field.getRaw(obj)
+            value = str(wrapper)
+            fname = wrapper.getFilename()
+            if fname is None:
+                fname = ''
+            ct = wrapper.getContentType()
+            if ct is None:
+                ct = ''
+        else:
+            # temporarily:
+            # dirty call, I know, just lazy to get method arguments
+            # TextField overrided getBaseUnit method but didn't follow API
+            try:
+                base_unit = field.getBaseUnit(obj, full=True)
+            except TypeError:
+                base_unit = field.getBaseUnit(obj)
+            fname = base_unit.getFilename()
+            ct = base_unit.getContentType()
+            value = base_unit.getRaw()
+
+        return fname, ct, value
 
 
 class CSVWriterSection(object):
